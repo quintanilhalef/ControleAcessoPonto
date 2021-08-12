de630794c6bd2e75b97e209fabecfec109594adc
Álef Quintanilha <39424996+quintanilhalef@users.noreply.github.com>
2021-08-12 14:52:34 -0300

    Subindo protótipo finalizado.

---
+++ src/main/java/com/dio/live/Service.java
@@ -0,0 +1,4 @@
+package com.dio.live;
+
+public class Service {
+}
---
+++ src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -0,0 +1,48 @@
+package com.dio.live.controller;
+
+import com.dio.live.model.JornadaTrabalho;
+import com.dio.live.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jornada")
+public class JornadaTrabalhoController {
+    @Autowired
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaList(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/idJornada")
+    public JornadaTrabalho getJornadaById(@PathVariable("idJornada") Long idJornada) throws Exception{
+        return jornadaService.getById(idJornada).orElseThrow(() -> new Exception("Jornada não encontrada"));
+    }
+
+    @PutMapping
+    public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @DeleteMapping("/{idJornada}")
+    public JornadaTrabalho DeleteJornadaById(@PathVariable("idJornada") Long idJornada) throws Exception{
+        try {
+            jornadaService.DeleteById(idJornada);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return (JornadaTrabalho) ResponseEntity.ok();
+    }
+
+
+}
